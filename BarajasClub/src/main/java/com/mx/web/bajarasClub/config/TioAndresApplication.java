4d73ca4a6a88997cf97eb1628a2dbf031599a0a9
RichardMata <88350317+RichardMata@users.noreply.github.com>
2025-09-30 17:48:34 -0600

    pruebas de rutas imagenes

--- BarajasClub/src/main/java/com/mx/web/bajarasClub/config/TioAndresApplication.java
+++ BarajasClub/src/main/java/com/mx/web/bajarasClub/config/TioAndresApplication.java
@@ -1,12 +1,15 @@
 package com.mx.web.bajarasClub.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -20,12 +23,13 @@ public class TioAndresApplication extends ResponseEntityExceptionHandler impleme
     }
     
     
-    @Value("${app.upload-dir}") private String uploadDir;
-    @Override
-    public void addResourceHandlers(org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry registry) {
-      registry.addResourceHandler("/uploads/**")
-              .addResourceLocations("file:" + uploadDir + "/");
-    }
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      Path uploadDir = Paths.get("uploads");
+      String uploadPath = uploadDir.toFile().getAbsolutePath();
+      registry.addResourceHandler("/app/uploads/**")
+              .addResourceLocations("file:" + uploadPath + "/");
+  }
     
     
 //    @Override
